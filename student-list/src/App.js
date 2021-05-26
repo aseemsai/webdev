79131232046e4f43e9ba1247dafe9e9f62ac6fcd
Aseem Panigrahi <aseemsai07@gmail.com>
2021-05-26 09:35:18 +0530

    Adding student-list project

---
+++ student-list/src/App.js
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [myList, setMyList] = useState([
+    {
+      id: 1,
+      name: 'Aseem',
+      age: 27
+    },
+    {
+      id: 2,
+      name: 'Alisha',
+      age: 28
+    },
+    {
+      id: 3,
+      name: 'Ronit',
+      age: 28
+    }
+  ])
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
+  function handleDelete(id) {
+
+    setMyList(
+      myList.filter((item) => item.id !== id))
+  };
+
+  // console.log(name, age)
+  function handleSubmit(e) {
+    //let newList = [...myList, {id:myList.length+1,name:name,age:age}]
+    let newList = [...myList]
+    newList.push({ id: myList.length + 1, name: name, age: age })
+    setMyList(newList)
+    setName('')
+    setAge('')
+    console.log(myList)
+  }
+  return (
+    <div className="App">
+      <h1>Student List</h1>
+      <div className="container">
+        <div className="student-table">
+          <table style={{ width: '80%' }}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                myList.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>{item.age}</td>
+                      <td><input type="button" className="btn" value='Delete' onClick={() => { handleDelete(item.id) }}></input></td>
+                    </tr>)
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+        <div className="add-form">
+          <form>
+            <h3>Enter Student Details</h3><br />
+            <strong>Name:</strong><br /><input type="text" name="name" id="name" value={name} onChange={(e) => { setName(e.target.value) }} /><br />
+            <strong>Age:</strong><br /><input type="number" name="age" id="age" value={age} onChange={(e) => { setAge(e.target.value) }} /><br />
+            <button type="submit" className="btn" onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
